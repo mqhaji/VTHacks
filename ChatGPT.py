fe421db37170a3c32c477434bf74ca7768629a29
Musa Haji <mqhaji@vt.edu>
2023-09-16 17:37:53 -0400

    reverted to using config json, changed text index

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,24 +3,10 @@ import json
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-API_KEY = "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"
-messages = [
-    {
-        "role": "system",
-        "content": "You are a diversity and inclusion consultant."
-    },
-    {
-        "role": "user",
-        "content": "Please evaluate our job listing for the given job position and provide feedback on how to make it more diverse and inclusive."
-    },
-    {
-        "role": "assistant",
-        "content": "Sure, please paste the job listing text here for evaluation."
-    }
-]
-model = "gpt-3.5-turbo"
+API_KEY = ""
+messages = []
+model = ""
 
-"""
 try:
     with open('config.json', 'r') as config_json:
         config = json.load(config_json)
@@ -35,7 +21,6 @@ except FileNotFoundError:
     pass
 except json.decoder.JSONDecodeError:
     pass
-"""
 
 openai.api_key = API_KEY
 
