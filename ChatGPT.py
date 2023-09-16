11f485c72f295081b8677307a0f4b38784e19e7e
Musa Haji <mqhaji@vt.edu>
2023-09-16 15:48:09 -0400

    hardcoded values into ChatGPT.py, added .gitignore

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,10 +3,24 @@ import json
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-API_KEY = ''
-messages = []
-model = ''
+API_KEY = "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"
+messages = [
+    {
+        "role": "system",
+        "content": "You are a diversity and inclusion consultant."
+    },
+    {
+        "role": "user",
+        "content": "Please evaluate our job listing for the given job position and provide feedback on how to make it more diverse and inclusive."
+    },
+    {
+        "role": "assistant",
+        "content": "Sure, please paste the job listing text here for evaluation."
+    }
+]
+model = "gpt-3.5-turbo"
 
+"""
 try:
     with open('config.json', 'r') as config_json:
         config = json.load(config_json)
@@ -21,25 +35,32 @@ except FileNotFoundError:
     pass
 except json.decoder.JSONDecodeError:
     pass
+"""
 
 openai.api_key = API_KEY
 
 print(API_KEY)
 print(messages)
 
-new_message = input("User : ")
-if new_message:
+my_message = input("Enter your job listing : ")
+
+
+def new_message():
     messages.append(
-        {"role": "user", "content": new_message},
+        {"role": "user", "content": my_message},
     )
 
-    print("Processing your request...")  # This will display while the API is processing
+    # This will display while the API is processing
+    print("Processing your request...")
 
     chat = openai.ChatCompletion.create(
         model=model, messages=messages
     )
 
     print("Response received!")  # This will display once the API responds
+
+
+new_message()
 
 reply = chat.choices[0].message.content
 print(f"ChatGPT: {reply}")
