5f1a1b64ad7a906d0a48cec698326bf5fad33dd6
EmilSalazar <emilbaezsalazar@gmail.com>
2023-09-17 00:44:45 -0400

    Added text input for api key

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sys  # for command line arguments
 app = Flask(__name__)
 
 # openai api key
-openai.api_key = str(sys.argv[1])
+# openai.api_key = str(sys.argv[1])
 
 @app.route('/')
 def index():
@@ -13,13 +13,17 @@ def index():
 
 @app.route('/run_python', methods=['POST'])
 def run_python():
+    api_key = request.form.get('api_key')  # Get the API key from the form
     user_input = request.form.get('user_input')
+
+    # Set the API key for OpenAI
+    openai.api_key = api_key
 
     # Create a conversation with user input
     messages = [{"role": "system", "content": "You are an intelligent assistant."}]
     messages.append({"role": "user", "content": user_input})
 
-    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     assistant_reply = chat.choices[0].message.content
     
     return jsonify({"response": assistant_reply})
